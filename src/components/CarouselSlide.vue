db7691218d813738a4393af491d5648fb5537f1b
TH02 <hyri05150@gmail.com>
2022-08-10 11:50:25 +0900

    First commit

---
+++ src/components/CarouselSlide.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-show="visibleSlide === index" class="carousel-slide">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['visibleSlide','index'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
