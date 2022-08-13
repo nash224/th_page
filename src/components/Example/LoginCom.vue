44db261339923e5abbe86b7733b543ffd011ef3f
nash224 <hyri0510@gmail.com>
2022-08-14 08:22:35 +0900

    Add contents

---
+++ src/components/Example/LoginCom.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login-wrapper">
+        <div class="login-title">Login form</div>
+        <form action="#">
+            <input type="text" id="login">
+            <label for=""></label>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
